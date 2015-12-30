03d68407c9c6831128bf1da410936b5ef8695894
Asa Downs <asadowns@gmail.com>
2015-12-29 17:34:30 -0800

    First Working Model

--- cleanmodel.R
+++ cleanmodel.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(SnowballC)
 library(ggplot2)  
 library(R.utils)
+library(stringr)
 setwd("~/RProg/DSSCapstone")
 
 if (!file.exists('Coursera-SwiftKey.zip')) {
@@ -15,13 +16,15 @@ if (!file.exists('Coursera-SwiftKey.zip')) {
 if (!file.exists('raw')) {
   unzip('Coursera-SwiftKey.zip', exdir="./raw")
 }
-
-if (!file.exists('1percent')) {
+dirname <- 'full'
+percent <- 1
+if (!file.exists(dirname)) {
+  dir.create(dirname)
   twitterFile <- file("raw/final/en_US/en_US.twitter.txt", "r")
   twitter <- readLines(twitterFile)
   close(twitterFile)
-  sampledTwitter <- sample(twitter,length(twitter)*0.01)
-  output<-file("1percent/twitter.txt")
+  sampledTwitter <- sample(twitter,length(twitter)*percent)
+  output<-file(paste0(dirname,"/twitter.txt"))
   writeLines(sampledTwitter, output)
   close(output)
   twitterLines <- countLines("raw/final/en_US/en_US.twitter.txt")[1]
@@ -30,8 +33,8 @@ if (!file.exists('1percent')) {
   blogsFile <- file("raw/final/en_US/en_US.blogs.txt", "r")
   blogs <- readLines(blogsFile)
   close(blogsFile)
-  sampledblogs <- sample(blogs,length(blogs)*0.01)
-  output<-file("1percent/blogs.txt")
+  sampledblogs <- sample(blogs,length(blogs)*percent)
+  output<-file(paste0(dirname,"/blogs.txt"))
   writeLines(sampledblogs, output)
   close(output)
   blogsLines <- countLines("raw/final/en_US/en_US.blogs.txt")[1]
@@ -40,35 +43,31 @@ if (!file.exists('1percent')) {
   newsFile <- file("raw/final/en_US/en_US.news.txt", "r")
   news <- readLines(newsFile)
   close(newsFile)
-  samplednews <- sample(news,length(news)*0.01)
-  output<-file("1percent/news.txt")
+  samplednews <- sample(news,length(news)*percent)
+  output<-file(paste0(dirname,"/news.txt"))
   writeLines(samplednews, output)
   close(output)
   newsLines <- countLines('raw/final/en_US/en_US.news.txt')[1]
   newsMax <- max(nchar(news))
 }
 
-source <- DirSource("1percent/") #input path for documents
-dbName <- "onepercentcorpus"
+source <- DirSource(dirname) #input path for documents
+dbName <- paste0(dirname,"corpus")
 if(!file.exists(dbName)){
   corpus <- PCorpus(source, readerControl=list(reader=readPlain,language="en"), dbControl=list(useDb=TRUE,dbName=dbName, dbType="DB1"))
+  stopwordsProfane <- "2g1c, 2 girls 1 cup, acrotomophilia, alabama hot pocket, alaskan pipeline, anal, anilingus, anus, apeshit, arsehole, ass, asshole, assmunch, auto erotic, autoerotic, babeland, baby batter, baby juice, ball gag, ball gravy, ball kicking, ball licking, ball sack, ball sucking, bangbros, bareback, barely legal, barenaked, bastardo, bastinado, bbw, bdsm, beaner, beaners, beaver cleaver, beaver lips, bestiality, big black, big breasts, big knockers, big tits, bimbos, birdlock, bitch, bitches, black cock, blonde action, blonde on blonde action, blowjob, blow job, blow your load, blue waffle, blumpkin, bollocks, bondage, boner, boob, boobs, booty call, brown showers, brunette action, bukkake, bulldyke, bullet vibe, bullshit, bung hole, bunghole, busty, butt, buttcheeks, butthole, camel toe, camgirl, camslut, camwhore, carpet muncher, carpetmuncher, chocolate rosebuds, circlejerk, cleveland steamer, clit, clitoris, clover clamps, clusterfuck, cock, cocks, coprolagnia, coprophilia, cornhole, coon, coons, creampie, cum, cumming, cunnilingus, cunt, darkie, date rape, daterape, deep throat, deepthroat, dendrophilia, dick, dildo, dirty pillows, dirty sanchez, doggie style, doggiestyle, doggy style, doggystyle, dog style, dolcett, domination, dominatrix, dommes, donkey punch, double dong, double penetration, dp action, dry hump, dvda, eat my ass, ecchi, ejaculation, erotic, erotism, escort, ethical slut, eunuch, faggot, fecal, felch, fellatio, feltch, female squirting, femdom, figging, fingerbang, fingering, fisting, foot fetish, footjob, frotting, fuck, fuck buttons, fucking, fudge packer, fudgepacker, futanari, gang bang, gay sex, genitals, giant cock, girl on, girl on top, girls gone wild, goatcx, goatse, god damn, gokkun, golden shower, goodpoop, goo girl, goregasm, grope, group sex, g-spot, guro, hand job, handjob, hard core, hardcore, hentai, homoerotic, honkey, hooker, hot carl, hot chick, how to kill, how to murder, huge fat, humping, incest, intercourse, jack off, jail bait, jailbait, jelly donut, jerk off, jigaboo, jiggaboo, jiggerboo, jizz, juggs, kike, kinbaku, kinkster, kinky, knobbing, leather restraint, leather straight jacket, lemon party, lolita, lovemaking, make me come, male squirting, masturbate, menage a trois, milf, missionary position, motherfucker, mound of venus, mr hands, muff diver, muffdiving, nambla, nawashi, negro, neonazi, nigga, nigger, nig nog, nimphomania, nipple, nipples, nsfw images, nude, nudity, nympho, nymphomania, octopussy, omorashi, one cup two girls, one guy one jar, orgasm, orgy, paedophile, paki, panties, panty, pedobear, pedophile, pegging, penis, phone sex, piece of shit, pissing, piss pig, pisspig, playboy, pleasure chest, pole smoker, ponyplay, poof, poon, poontang, punany, poop chute, poopchute, porn, porno, pornography, prince albert piercing, pthc, pubes, pussy, queaf, queef, quim, raghead, raging boner, rape, raping, rapist, rectum, reverse cowgirl, rimjob, rimming, rosy palm, rosy palm and her 5 sisters, rusty trombone, sadism, santorum, scat, schlong, scissoring, semen, sex, sexo, sexy, shaved beaver, shaved pussy, shemale, shibari, shit, shitty, shota, shrimping, skeet, slanteye, slut, s&m, smut, snatch, snowballing, sodomize, sodomy, spic, splooge, splooge moose, spooge, spread legs, spunk, strap on, strapon, strappado, strip club, style doggy, suck, sucks, suicide girls, sultry women, swastika, swinger, tainted love, taste my, tea bagging, threesome, throating, tied up, tight white, tit, tits, titties, titty, tongue in a, topless, tosser, towelhead, tranny, tribadism, tub girl, tubgirl, tushy, twat, twink, twinkie, two girls one cup, undressing, upskirt, urethra play, urophilia, vagina, venus mound, vibrator, violet wand, vorarephilia, voyeur, vulva, wank, wetback, wet dream, white power, wrapping men, wrinkled starfish, xx, xxx, yaoi, yellow showers, yiffy, zoophilia, 🖕"
+  #stopwordsProfane <- tolower(c(stopwords('en'),stopwordsProfane))
+  corpus <- tm_map(corpus,removeNumbers)
+  corpus <- tm_map(corpus,removePunctuation)
+  corpus <- tm_map(corpus,content_transformer(tolower))
+  corpus <- tm_map(corpus, removeWords,stopwordsProfane)
+  corpus <- tm_map(corpus,stripWhitespace)
 } else {
   db <- dbInit(dbName, "DB1")
   files <- dbLoad(db)
-  blogs <- dbFetch(db,'blogs.txt')
-  twitter <- dbFetch(db,'twitter.txt')
-  news <- dbFetch(db,'news.txt')
-  corpus <- c(blogs,twitter,news)
+  corpus <- as.VCorpus(dbMultiFetch(db,files))
 }
-
-profane <- "2g1c, 2 girls 1 cup, acrotomophilia, alabama hot pocket, alaskan pipeline, anal, anilingus, anus, apeshit, arsehole, ass, asshole, assmunch, auto erotic, autoerotic, babeland, baby batter, baby juice, ball gag, ball gravy, ball kicking, ball licking, ball sack, ball sucking, bangbros, bareback, barely legal, barenaked, bastardo, bastinado, bbw, bdsm, beaner, beaners, beaver cleaver, beaver lips, bestiality, big black, big breasts, big knockers, big tits, bimbos, birdlock, bitch, bitches, black cock, blonde action, blonde on blonde action, blowjob, blow job, blow your load, blue waffle, blumpkin, bollocks, bondage, boner, boob, boobs, booty call, brown showers, brunette action, bukkake, bulldyke, bullet vibe, bullshit, bung hole, bunghole, busty, butt, buttcheeks, butthole, camel toe, camgirl, camslut, camwhore, carpet muncher, carpetmuncher, chocolate rosebuds, circlejerk, cleveland steamer, clit, clitoris, clover clamps, clusterfuck, cock, cocks, coprolagnia, coprophilia, cornhole, coon, coons, creampie, cum, cumming, cunnilingus, cunt, darkie, date rape, daterape, deep throat, deepthroat, dendrophilia, dick, dildo, dirty pillows, dirty sanchez, doggie style, doggiestyle, doggy style, doggystyle, dog style, dolcett, domination, dominatrix, dommes, donkey punch, double dong, double penetration, dp action, dry hump, dvda, eat my ass, ecchi, ejaculation, erotic, erotism, escort, ethical slut, eunuch, faggot, fecal, felch, fellatio, feltch, female squirting, femdom, figging, fingerbang, fingering, fisting, foot fetish, footjob, frotting, fuck, fuck buttons, fucking, fudge packer, fudgepacker, futanari, gang bang, gay sex, genitals, giant cock, girl on, girl on top, girls gone wild, goatcx, goatse, god damn, gokkun, golden shower, goodpoop, goo girl, goregasm, grope, group sex, g-spot, guro, hand job, handjob, hard core, hardcore, hentai, homoerotic, honkey, hooker, hot carl, hot chick, how to kill, how to murder, huge fat, humping, incest, intercourse, jack off, jail bait, jailbait, jelly donut, jerk off, jigaboo, jiggaboo, jiggerboo, jizz, juggs, kike, kinbaku, kinkster, kinky, knobbing, leather restraint, leather straight jacket, lemon party, lolita, lovemaking, make me come, male squirting, masturbate, menage a trois, milf, missionary position, motherfucker, mound of venus, mr hands, muff diver, muffdiving, nambla, nawashi, negro, neonazi, nigga, nigger, nig nog, nimphomania, nipple, nipples, nsfw images, nude, nudity, nympho, nymphomania, octopussy, omorashi, one cup two girls, one guy one jar, orgasm, orgy, paedophile, paki, panties, panty, pedobear, pedophile, pegging, penis, phone sex, piece of shit, pissing, piss pig, pisspig, playboy, pleasure chest, pole smoker, ponyplay, poof, poon, poontang, punany, poop chute, poopchute, porn, porno, pornography, prince albert piercing, pthc, pubes, pussy, queaf, queef, quim, raghead, raging boner, rape, raping, rapist, rectum, reverse cowgirl, rimjob, rimming, rosy palm, rosy palm and her 5 sisters, rusty trombone, sadism, santorum, scat, schlong, scissoring, semen, sex, sexo, sexy, shaved beaver, shaved pussy, shemale, shibari, shit, shitty, shota, shrimping, skeet, slanteye, slut, s&m, smut, snatch, snowballing, sodomize, sodomy, spic, splooge, splooge moose, spooge, spread legs, spunk, strap on, strapon, strappado, strip club, style doggy, suck, sucks, suicide girls, sultry women, swastika, swinger, tainted love, taste my, tea bagging, threesome, throating, tied up, tight white, tit, tits, titties, titty, tongue in a, topless, tosser, towelhead, tranny, tribadism, tub girl, tubgirl, tushy, twat, twink, twinkie, two girls one cup, undressing, upskirt, urethra play, urophilia, vagina, venus mound, vibrator, violet wand, vorarephilia, voyeur, vulva, wank, wetback, wet dream, white power, wrapping men, wrinkled starfish, xx, xxx, yaoi, yellow showers, yiffy, zoophilia, 🖕"
-stopwordsProfane <- tolower(c(stopwords('en'),profane))
 NgramTokenizer <- function(x, n) unlist(lapply(ngrams(words(x),n), paste, collapse = " "), use.names = FALSE)
-corpus <- tm_map(corpus,removeNumbers)
-corpus <- tm_map(corpus,removePunctuation)
-corpus <- tm_map(corpus,content_transformer(tolower))
-corpus <- tm_map(corpus, removeWords,stopwordsProfane)
-corpus <- tm_map(corpus,stripWhitespace)
 
 dtmUnagram <- DocumentTermMatrix(corpus)
 freq <- sort(colSums(as.matrix(dtmUnagram)), decreasing=TRUE)
@@ -130,26 +129,55 @@ plot5
 gramFreq <- list(freqBigram,freqTrigram,freq4gram,freq5gram)
 
 predictNgram <- function(sentence) {
+  
+  maxLength <- 3
+  
   processed <- sentence %>% removeNumbers %>%
     removePunctuation() %>%
     tolower() %>%
     removeWords(stopwordsProfane) %>%
     stripWhitespace() %>%
     trim()
-  print(processed)
+  
+  rmStopwords <- processed %>% 
+    removeWords(stopwords("english")) %>%
+    stripWhitespace() %>%
+    trim()
+  
+  reducePattern <- str_replace_all(rmStopwords, "\\s+","|")
+  
+  values <- tm_index(corpus, FUN=function(x) grep(reducePattern, content(x),value=TRUE))
+  values <- as.matrix(values)
+  values <- VCorpus(VectorSource(values))
+  
+#  for (i in 2:(pmaxLength+1)) {
+#    dtm <- DocumentTermMatrix(values, list(tokenize = function(x) NgramTokenizer(x,i)))
+#    freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)
+#    
+#    if (exists('gramFreq')) {
+#      gramFreq[[i-1]] <- freq
+#    } else {
+#      gramFreq <- list()
+#      gramFreq[[1]] <- freq
+#    }
+#  }
+  
   strlength <- str_count(processed,"\\w+")
-  if (strlength>4) strlength <- 4
+  
+  if (strlength>maxLength) strlength <- maxLength
+  tokenLength <- strlength + 1
   pattern <- paste(rep("\\w+\\s+",strlength-1),collapse = "",sep="")
   pattern <- paste0(pattern,"\\w+$")
   predictor <- str_extract(processed,pattern)
   print(predictor)
-  gram <- gramFreq[[strlength]]
-  print(strlength-1)
+  dtm <- DocumentTermMatrix(values, list(tokenize = function(x) NgramTokenizer(x,tokenLength)))
+  gram <- sort(col_sums(dtm), decreasing=TRUE)
+  
   phraseMatch <- names(which.max(gram[grepl(paste0("^",predictor), names(gram))]))
   print(phraseMatch)
   if (!is.null(phraseMatch)) {
     match <- str_extract(phraseMatch,'\\w+$')
-    return(match)
+    match
   }
   else if (strlength>1){
     pattern <- paste(replicate(strlength-2, "\\w+\\s+"), collapse = "")
@@ -161,37 +189,3 @@ predictNgram <- function(sentence) {
     print('no prediction')
   }
 }
-
-
-predictSentence <- function(sentence) {
-  processed <- sentence %>% removeNumbers %>%
-    removePunctuation() %>%
-    tolower() %>%
-    removeWords(stopwordsProfane) %>%
-    stripWhitespace() %>%
-    trim()
-  print(processed)
-  strlength <- str_count(processed,"\\w+")
-    if (strlength>2) {
-      pattern <- "\\w+\\s+\\w+\\s+\\w+$"
-      predictor <- str_extract(processed,pattern)
-      print(predictor)
-      phraseMatch <- names(which.max(freq4gram[grepl(paste0("^",predictor), names(freq4gram))]))
-      print(phraseMatch)
-      match <- str_extract(phraseMatch,'\\w+$')
-    }
-    if (strlength>1) {
-      pattern <- "\\w+\\s+\\w+$"
-      predictor <- str_extract(processed,pattern)
-      phraseMatch <- names(which.max(freqTrigram[grepl(paste0("^",predictor), names(freqTrigram))]))
-    }
-    if (strlength>0) {
-      pattern <- "\\w+$"
-      predictor <- str_extract(processed,pattern)
-      phraseMatch <- names(which.max(freqBigram[grepl(paste0("^",predictor), names(freqBigram))]))
-    }
-  match <- str_extract(phraseMatch,'\\w+$')
-match
-
-}
-
